0c965410c86cc51e6ebc2e5170e8b4abb27084f7
Gala <GalkaS@users.noreply.github.com>
2018-10-10 22:43:57 -0400

    Update events pagination and adjust some css (#5)

--- src/ForksList.js
+++ src/ForksList.js
@@ -1,21 +1,33 @@
 import React from "react";
 import Fork from "./Fork";
 
-const ForksList = ({ forks }) => (
-  <React.Fragment>
-    <h2 className="sectionTitle">Your forked repos:</h2>
-    <ul>
-      {forks.map((fork, index) => (
-        <Fork
-          key={`fork-${index}`}
-          name={fork.repo.name}
-          forkUrl={fork.payload.forkee.html_url}
-          originalUrl={`https://github.com/${fork.repo.name}`}
-          forkName={fork.payload.forkee.full_name}
-        />
-      ))}
-    </ul>
-  </React.Fragment>
-);
+const ForksList = ({ forks }) => {
+  if (forks.length === 0) {
+    return (
+      <React.Fragment>
+        <h2 className="sectionTitle">Your forked repos:</h2>
+        <p className="no-info-message">
+          Sorry, you don't have any forked repos!
+        </p>
+      </React.Fragment>
+    );
+  }
+  return (
+    <React.Fragment>
+      <h2 className="sectionTitle">Your forked repos:</h2>
+      <ul>
+        {forks.map((fork, index) => (
+          <Fork
+            key={`fork-${index}`}
+            name={fork.repo.name}
+            forkUrl={fork.payload.forkee.html_url}
+            originalUrl={`https://github.com/${fork.repo.name}`}
+            forkName={fork.payload.forkee.full_name}
+          />
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
 
 export default ForksList;
--- src/TextField.js
+++ src/TextField.js
@@ -4,6 +4,7 @@ const TextField = ({ value, handleChange, label, id, name }) => (
   <React.Fragment>
     <label htmlFor={id}>{label}</label>
     <input
+      className="text-field"
       type="text"
       value={value}
       onChange={handleChange}
